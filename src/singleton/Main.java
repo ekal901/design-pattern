d3f2b1b2e457fb5ca7fe2d55ef0487893a01b2d2
damhee kim <ekal.kdh@gmail.com>
2021-04-02 17:26:33 +0900

    singleton main added

--- src/singleton/Main.java
+++ src/singleton/Main.java
@@ -2,6 +2,7 @@ package singleton;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello Singleton");
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton);
     }
 }
